769d7734885ae2d6bdc9a62b304e805bf1432fc7
lyyking <1172037796@qq.com>
2026-09-21 20:28:17 +0800

    忽略文件

---
+++ devkit/devkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  devkit
+//
+//  Created by cai on 2026/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ devkit/devkit/devkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  devkitApp.swift
+//  devkit
+//
+//  Created by cai on 2026/9/21.
+//
+
+import SwiftUI
+
+@main
+struct devkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
